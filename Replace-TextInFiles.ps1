0f42cbdf4046bca82a91584f95c387df3355c7b7
Lukasz Siwek <xofczarx@gmail.com>
2017-05-16 16:56:19 +0200

    Help and path validataion were added to Replace-TextInFiles

--- Replace-TextInFiles.ps1
+++ Replace-TextInFiles.ps1
@@ -1,4 +1,34 @@
+<#
+.SYNOPSIS
+Script allows to change given text with anoter text in files under given location.
+
+.DESCRIPTION 
+The script searches for files which contians text to be replaced under given location and then changes all occures with new text. 
+Effected files are listed in log.txt files and errors are listed in errorLog.txt.
+
+.PARAMETER Path
+Path to destination where text in files should be changed. Script changes text recursively. Default value is '.'
+
+.PARAMETER OldText
+Text which should be replaced.
+
+.PARAMETER NewText
+New text.
+
+.PARAMETER FileExtensions
+List of file extensions which should be included in text replacing. 
+
+.EXAMPLE
+./Replace-TextInFiles.ps1 -NewText 'value1' -OldText 'value2'  -> in all files recursively in the same directory where script is (.) text "value1" will be replaced by "value2"
+.EXAMPLE
+./Replace-TextInFiles.ps1 -Path 'C:\MyFolder' -NewText 'value1' -OldText 'value2' -> in all files recursively in directory "C:\MyFolder" text "value1" will be replaced by "value2"
+.EXAMPLE
+./Replace-TextInFiles.ps1 -Path 'C:\MyFolder' -NewText 'value1' -OldText 'value2' -FileExtensions 'cs','aspx' -> in .cs and .aspx files recursively in directory "C:\MyFolder" text "value1" will be replaced by "value2"
+
+#>
+
 param(
+    [ValidateScript({Test-Path $_})]
     [string]$Path = '.',
     [Parameter(Mandatory)]
     [string]$OldText,
@@ -15,6 +45,7 @@ New-Item $logFilePath -type file -Force
 New-Item $errorLogFilePath -type file -Force
 
 
+
 foreach($fileExtension in $FileExtensions){
     $filesToReplace.Add("*.$fileExtension") | Out-Null
 }
@@ -29,7 +60,7 @@ foreach($file in $files)
 {   
     try {
         (Get-Content $file).replace($OldText,$NewText) | Set-Content $file
-        $succededFiles.Add($file)
+        $succededFiles.Add($file) | Out-Null
     } catch {
         "It wan't possible to change text in '$file'. Reason $($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)" | Out-File $errorLogFilePath
     }
@@ -41,4 +72,4 @@ $succededFiles | Out-File $logFilePath -Append
 $logFullPath = Resolve-Path $logFilePath
 $errorLogFullPath = Resolve-Path $errorLogFilePath
 
-Write-Output "Replaceing was finished.\n Log file can be found in '$logFullPath'.\n Errors are logged in '$errorLogFullPath'."
+Write-Output "Replacing was finished.`n Log file can be found in '$logFullPath'.`n Errors are logged in '$errorLogFullPath'."
